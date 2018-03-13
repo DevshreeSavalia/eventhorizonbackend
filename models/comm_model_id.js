09afafb39d0459a8e659ffedcd4fdecf711855d3
DevshreeSavalia <devshreesavalia@gmail.com>
2018-03-13 17:31:21 +0530

    comm_id,member route and model changed

--- models/comm_model_id.js
+++ models/comm_model_id.js
@@ -2,7 +2,7 @@ var db = require("../dbconnect");
 var com_id = {
   getCommunityByIdd: function(id, callback) {
     return db.query(
-      "select c.*,cr.*,u.* from tbl_community c,tbl_community_registration cr,tbl_user u where cr.comm_id_fk=c.comm_id and cr.email_id_fk=u.email_id and c.comm_id=?",
+      "select * from tbl_community where comm_id=?",
       [id],
       callback
     );
--- models/member_segmt_model.js
+++ models/member_segmt_model.js
@@ -2,7 +2,7 @@ var db = require("../dbconnect");
 var memsegmt = {
   getMembSegment: function([id],callback) {
     return db.query(
-      "select u.* from tbl_user as u,tbl_community as c,tbl_community_registration as com where com.comm_id_fk=c.comm_id and c.email_id_fk=u.email_id and c.comm_id_fk=?",
+      "select e.*,c.* from tbl_event e,tbl_community c where e.comm_id_fk=c.comm_id and c.comm_id",
       [id],callback
     );
   }
