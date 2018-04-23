c64a683a50bbd6c4690051e996f2797a38189d50
DevshreeSavalia <devshreesavalia@gmail.com>
2018-04-23 12:13:39 +0530

    final

--- models/profile_model.js
+++ models/profile_model.js
@@ -1,10 +1,9 @@
-var db=require("../dbconnect");
-var profile={
+var db = require("../dbconnect");
+var profile = {
 
-getProfile:function(id,callback){
+    getProfile: function (id, callback) {
 
-    return db.query("select u.*,er.*,e.* from tbl_user u,tbl_event e,tbl_event_registration er where er.e_id_fk=e.e_id and er.email_id_fk=u.email_id and u.email_id=? and e.e_enddate<CURDATE()",[id],callback);
-
+        return db.query("select u.*,er.*,e.* from tbl_user u,tbl_event e,tbl_event_registration er where er.e_id_fk=e.e_id and er.email_id_fk=u.email_id and u.email_id=? and e.e_enddate<CURDATE()", [id], callback);
+    }
 }
-}
-module.exports=profile;
+module.exports = profile;
--- models/upcom_page_model.js
+++ models/upcom_page_model.js
@@ -2,7 +2,7 @@ var db = require("../dbconnect");
 var uppage = {
   getUpPage: function(id, callback) {
     return db.query(
-      "select e.*,e_reg.* from tbl_event e,tbl_event_registration e_reg,tbl_user u where e_reg.e_id_fk=e.e_id and e_reg.email_id_fk=u.email_id and u.email_id=? and e.e_enddate>CURDATE()",
+      "select e.*,e_reg.*,u.* from tbl_event e,tbl_event_registration e_reg,tbl_user u where e_reg.e_id_fk=e.e_id and e_reg.email_id_fk=u.email_id and u.email_id=? and e.e_enddate>CURDATE()",
       [id],callback);
   }
 };
